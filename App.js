57f1856d5b35297870e131507cdea1fa75db8cd6
Colin Carter <colin_carter@fmail.co.uk>
2017-10-11 11:26:00 +0100

    Prettier-ifying

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//@flow
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default class App extends React.Component {
   render() {
@@ -16,8 +17,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
