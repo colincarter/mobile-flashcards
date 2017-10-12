abd04dce5024bb48eb0a2ad52e33179058a93b62
Colin Carter <colin_carter@fmail.co.uk>
2017-10-12 11:17:22 +0100

    Moving things around to get to project structure I'm happy with

---
+++ components/MobileFlashCards.js
@@ -0,0 +1,13 @@
+// @flow
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default class MobileFlashCards extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    );
+  }
+}
--- src/components/App.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
-export default class App extends React.Component {
-  render() {
-    return null;
-  }
-}
