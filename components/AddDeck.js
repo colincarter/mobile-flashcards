35465434d2f151138465c7bf3232c29b0df0d3b1
Colin Carter <colin_carter@fmail.co.uk>
2017-10-12 16:42:08 +0100

    WIP getting a new deck saved

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,11 +1,34 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TextInput, AsyncStorage, Button } from "react-native";
 
 export default class AddDeck extends React.Component {
+  deckKey = "decks";
+
+  state = {
+    text: ""
+  };
+
+  onChangeText = ({ text }) => {
+    this.setState({ text });
+  };
+
+  onButtonPress = async () => {
+    let decks = await AsyncStorage.getItem(this.deckKey);
+    if (decks === null) {
+      decks = [];
+    } else {
+      decks = JSON.parse(decks);
+    }
+    const newDecks = [...decks, this.state.text];
+    await AsyncStorage.setItem(JSON.stringify(this.deckKey, newDecks));
+  };
+
   render() {
     return (
       <View>
         <Text>Add Deck</Text>
+        <TextInput placeholder="Deck name" onChangeText={this.onChangeText} />
+        <Button onPress={this.onButtonPress} title="Add Deck" />
       </View>
     );
   }
