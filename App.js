3c5b48190622836e16744dc4fa98ab00161a5eaa
Colin Carter <colin_carter@fmail.co.uk>
2017-11-11 16:38:39 +0000

    Added notifications

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Provider } from "react-redux";
 import { StyleSheet, Text, View, AsyncStorage } from "react-native";
 import { StackNavigator, TabNavigator } from "react-navigation";
+import { Constants, Notifications, Permissions } from "expo";
 
 import AddDeck from "./components/AddDeck";
 import Decks from "./components/Decks";
@@ -54,6 +55,26 @@ const loadDecks = async store => {
 loadDecks(store);
 
 export default class App extends React.Component {
+  async componentDidMount() {
+    const notification = {
+      title: "Mobile Flashcards",
+      body: "Don't forget to take your quiz today"
+    };
+
+    const schedulingOptions = {
+      time: new Date().getTime() + 1000,
+      repeat: "day"
+    };
+
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    if (status === "granted") {
+      await Notifications.scheduleLocalNotificationAsync(
+        notification,
+        schedulingOptions
+      );
+    }
+  }
+
   render() {
     return (
       <Provider store={store}>
