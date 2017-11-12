fea853cf588c89c2fdeaaf9b7e06ddd1a2db0a8f
Colin Carter <colin_carter@fmail.co.uk>
2017-11-12 07:56:17 +0000

    Move setup notifications to its own file

--- App.js
+++ App.js
@@ -1,35 +1,19 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { StyleSheet, Text, View, AsyncStorage } from "react-native";
-import { Constants, Notifications, Permissions } from "expo";
 
 import configureStore from "./store/configureStore";
 import defaultState from "./store/defaultState";
 import loadDecks from "./lib/loadDecks";
+import setupNotifications from "./lib/setupNotifications";
 import MainNavigation from "./routes";
 
 const store = configureStore(defaultState);
 loadDecks(store);
 
 export default class App extends React.Component {
-  async componentDidMount() {
-    const notification = {
-      title: "Mobile Flashcards",
-      body: "Don't forget to take your quiz today"
-    };
-
-    const schedulingOptions = {
-      time: new Date().getTime() + 1000,
-      repeat: "day"
-    };
-
-    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-    if (status === "granted") {
-      await Notifications.scheduleLocalNotificationAsync(
-        notification,
-        schedulingOptions
-      );
-    }
+  componentDidMount() {
+    setupNotifications();
   }
 
   render() {
