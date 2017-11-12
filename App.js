e9163f01701efa16837c281271a3213e8f5e4ed1
Colin Carter <colin_carter@fmail.co.uk>
2017-11-12 07:52:50 +0000

    Moving loadDecks to its own file

--- App.js
+++ App.js
@@ -1,55 +1,14 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { StyleSheet, Text, View, AsyncStorage } from "react-native";
-import { StackNavigator, TabNavigator } from "react-navigation";
 import { Constants, Notifications, Permissions } from "expo";
 
-import AddDeck from "./components/AddDeck";
-import Decks from "./components/Decks";
-import DeckView from "./components/DeckView";
-import AddCard from "./components/AddCard";
-import Quiz from "./components/Quiz";
-
-import DeckStorage from "./lib/storage";
 import configureStore from "./store/configureStore";
 import defaultState from "./store/defaultState";
-import { addDeck, addCard } from "./actions";
-
-const Tabs = TabNavigator({
-  Decks: {
-    screen: Decks
-  },
-  AddDeck: {
-    screen: AddDeck
-  }
-});
-
-const MainNavigation = StackNavigator({
-  Home: {
-    screen: Tabs
-  },
-  DeckView: {
-    screen: DeckView
-  },
-  AddCard: {
-    screen: AddCard
-  },
-  Quiz: {
-    screen: Quiz
-  }
-});
+import loadDecks from "./lib/loadDecks";
+import MainNavigation from "./routes";
 
 const store = configureStore(defaultState);
-
-const loadDecks = async store => {
-  const ds = new DeckStorage();
-  const decks = await ds.getDecks();
-  for (const deck in decks) {
-    store.dispatch(addDeck(deck));
-    decks[deck].questions.forEach(card => store.dispatch(addCard(deck, card)));
-  }
-};
-
 loadDecks(store);
 
 export default class App extends React.Component {
